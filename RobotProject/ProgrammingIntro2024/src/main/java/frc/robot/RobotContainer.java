12cfc49c5c1576a139a61e51f256e564ce5eeb40
FRC-6201 <115188996+FRC-6201@users.noreply.github.com>
2024-09-26 16:21:30 -0400

    Filled out the to-dos and don'ts

--- RobotProject/ProgrammingIntro2024/src/main/java/frc/robot/RobotContainer.java
+++ RobotProject/ProgrammingIntro2024/src/main/java/frc/robot/RobotContainer.java
@@ -24,7 +24,7 @@ public class RobotContainer {
 
   // The robot's subsystems and commands are defined here...
   private final Drive drive = new Drive(controller::getLeftY, controller::getRightY);
-
+ 
   /**
    * The container for the robot. Contains subsystems, OI devices, and commands.
    */
@@ -32,6 +32,7 @@ public class RobotContainer {
     // Configure the trigger bindings
     configureBindings();
     // TODO: Set Default command for the Drive method, to the drive.driveCommand() command
+   drive.setDefaultCommand(drive.driveCommand());
   }
 
   /**
