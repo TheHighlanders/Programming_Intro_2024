97db110f508d8024bd05ed65c73e82d6a4ddc580
FRC-6201 <frc6201@gmail.com>
2024-10-03 16:27:30 -0400

    muahahahahahaha
    AHAHAHAHAHAHAHAHAMUAHAHAHAHAHAHAHAHAMUAHHAHAHAAHAHAHAHAHAMUAHAHAHAHAHAHAHAAHAH

--- RobotProject/ProgrammingIntro2024/src/main/java/frc/robot/subsystems/Subsubsystem.java
+++ RobotProject/ProgrammingIntro2024/src/main/java/frc/robot/subsystems/Subsubsystem.java
@@ -4,17 +4,20 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Motor;
 
 public class Subsubsystem extends SubsystemBase {
   /** Creates a new Subsubsystem. */
 
-  Motor ri = new Motor();
-  Motor le = new Motor();
+  CANSparkMax le = new CANSparkMax(3, MotorType.kBrushless);
+  CANSparkMax ri = new CANSparkMax(4, MotorType.kBrushless);
 
   public void rStartFlywheel() {
     ri.set(1);
@@ -43,15 +46,17 @@ public class Subsubsystem extends SubsystemBase {
         .finallyDo(() -> stop());
   }
 
+  RelativeEncoder leftEncoder;
+  RelativeEncoder rightEncoder;
+
   public Subsubsystem() {
+    leftEncoder = le.getEncoder();
+    rightEncoder = ri.getEncoder();
   }
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
-    ri.update(0.02);
-    le.update(0.02);
-    SmartDashboard.putNumber("right RPM", ri.getAngularVelocityRPM());
-    SmartDashboard.putNumber("left RPM", le.getAngularVelocityRPM());
+    SmartDashboard.putNumber("Left Motor Position", leftEncoder.getPosition());
+    SmartDashboard.putNumber("Right Motor Position", rightEncoder.getPosition());
   }
 }
