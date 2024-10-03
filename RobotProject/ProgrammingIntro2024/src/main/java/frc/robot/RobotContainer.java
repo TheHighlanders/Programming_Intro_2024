2725089b72049697b998f97429a5098d30eb520c
FRC-6201 <frc6201@gmail.com>
2024-10-03 16:29:20 -0400

    Shooter

--- RobotProject/ProgrammingIntro2024/src/main/java/frc/robot/RobotContainer.java
+++ RobotProject/ProgrammingIntro2024/src/main/java/frc/robot/RobotContainer.java
@@ -8,7 +8,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.PrintCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.subsystems.Drive;
-import frc.robot.subsystems.Subsubsystem;
+import frc.robot.subsystems.Shooter;
 
 /**
  * This class is where the bulk of the robot should be declared. Since
@@ -25,7 +25,7 @@ public class RobotContainer {
 
   // The robot's subsystems and commands are defined here...
   private final Drive drive = new Drive(controller::getRightY, controller::getRightX);
-  private final Subsubsystem luncher = new Subsubsystem();
+  private final Shooter luncher = new Shooter();
   /**
    * The container for the robot. Contains subsystems, OI devices, and commands.
    */
