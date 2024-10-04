9d3f6e991f1b5685fc12e35a9d094d8616ec9810
FRC-6201 <frc6201@gmail.com>
2024-10-04 15:56:06 -0400

    forgot commit

--- RobotProject/ProgrammingIntro2024/src/main/java/frc/robot/RobotContainer.java
+++ RobotProject/ProgrammingIntro2024/src/main/java/frc/robot/RobotContainer.java
@@ -5,9 +5,11 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.PrintCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.subsystems.Drive;
+import frc.robot.subsystems.launcherSubsystem;
 
 /**
  * This class is where the bulk of the robot should be declared. Since
@@ -21,6 +23,7 @@ import frc.robot.subsystems.Drive;
 public class RobotContainer {
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandXboxController controller = new CommandXboxController(0);
+  private final launcherSubsystem launcher = new launcherSubsystem();
 
   // The robot's subsystems and commands are defined here...
   private final Drive drive = new Drive(controller::getLeftY, controller::getRightY);
@@ -38,7 +41,7 @@ public class RobotContainer {
    * Use this method to define your trigger->command mappings.
    */
   private void configureBindings() {
-
+controller.a().whileTrue(launcher.getLaunchCommand());
   }
 
   /**
