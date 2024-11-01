35d0fcd05bf9ff1c7cc9f7ae2fc9c1b329cbcfb1
FRC 6201 Driver Station Laptop <frc6201@gmail.com>
2024-11-01 16:44:53 -0400

    we did something cool

---
+++ PID Example/src/main/java/frc/robot/subsystems/Wheel.java
@@ -0,0 +1,76 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkBase.ControlType;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkPIDController;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Wheel extends SubsystemBase {
+  /** Creates a new Wheel. */
+
+  // Creates the wheelMotor field, the motor controller
+  private CANSparkMax wheelMotor;
+  //Creates the wheelPID field, a PID controller to control the wheel
+  private SparkPIDController wheelPID;
+
+  //PID Controller constants!
+  private double p = 0.1;
+  private double i = 0;
+  private double d = 0.01;
+  private double tolerance = 1;
+  private double setpoint = 0;
+
+  RelativeEncoder wheelEncoder;
+  public Wheel() {
+    // Populates the wheelMotor field by creating a new CANSparkMax
+    wheelMotor = new CANSparkMax(2, MotorType.kBrushless);
+
+    // Populates the wheelPID field by getting the PID Controller of the motor controller
+    wheelPID = wheelMotor.getPIDController();
+  
+    wheelPID.setP(p);
+    wheelPID.setI(i);
+    wheelPID.setD(d);
+
+    wheelEncoder = wheelMotor.getEncoder();
+    wheelEncoder.setPositionConversionFactor(28.125);
+    wheelEncoder.setPosition(0);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    SmartDashboard.putNumber("Position", wheelEncoder.getPosition());
+    SmartDashboard.putNumber("Setpoint", setpoint);
+    
+    SmartDashboard.putBoolean("Condition", Math.abs(wheelEncoder.getPosition()-setpoint) <= tolerance);
+    if(Math.abs(wheelEncoder.getPosition()-setpoint) <= tolerance) {
+      wheelPID.setReference(0,ControlType.kDutyCycle);
+      wheelPID.setP(0);
+      wheelPID.setI(0);
+      wheelPID.setD(0);
+    }
+
+  }
+
+  public Command setpointCMD(double setpointDeg){
+    return new RunCommand(()->{
+      wheelPID.setP(p);
+      wheelPID.setI(i);
+      wheelPID.setD(d);
+      wheelPID.setReference(setpointDeg, ControlType.kPosition);
+      setpoint = setpointDeg;
+      }
+    );
+  }
+}
