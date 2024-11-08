d6312d1d8fdf16541d1ae26200619e1c2422959b
potsofbeans <118775526+potsofbeans@users.noreply.github.com>
2024-11-08 16:02:28 -0500

    Revert "Merge branch 'Simon' into Lloyd ( maybe broken)"
    This reverts commit d436e552cbb9c03486ec97a3dea083e848c0c365.

--- RobotProject/ProgrammingIntro2024/src/main/java/frc/robot/subsystems/Drive.java
+++ RobotProject/ProgrammingIntro2024/src/main/java/frc/robot/subsystems/Drive.java
@@ -13,39 +13,59 @@ import com.revrobotics.CANSparkLowLevel.MotorType;
 
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Motor;
 
 public class Drive extends SubsystemBase {
-  CANSparkMax left;
-  CANSparkMax right;
-  
+  // TODO: Declare some Motor Objects, one for each side of the robot drivetrain
+
+  CANSparkMax right1;
+  CANSparkMax left1;
+  CANSparkMax right2;
+  CANSparkMax left2;
+
+//  RelativeEncoder leftEncoder;
+//  RelativeEncoder rightEncoder;
 
   DoubleSupplier leftSupplier;
   DoubleSupplier rightSupplier;
   Field2d field = new Field2d();
   DifferentialDriveOdometry odo = new DifferentialDriveOdometry(new Rotation2d(0), 0, 0);
-  RelativeEncoder encoder;
-  SparkPIDController pid;
+
+  RelativeEncoder endoder1;
+
+  SparkPIDController pid1;
+
   /** Creates a new Drive. */
   public Drive(DoubleSupplier leftSupplier, DoubleSupplier rightSupplier) {
-   left = new CANSparkMax(1, MotorType.kBrushed );
-   right = new CANSparkMax(2, MotorType.kBrushed );
-   
-   pid = left.getPIDController();
-   encoder = left.getEncoder();
-   pid.setP(1);
-   pid.setD(Math.PI);
+    // TODO: Initialize Motor Objects
+    left1 = new CANSparkMax(2, MotorType.kBrushed);
+    right1 = new CANSparkMax(1, MotorType.kBrushed);
+    left2 = new CANSparkMax(3, MotorType.kBrushed);
+    right2 = new CANSparkMax(4, MotorType.kBrushed);
+    left2.follow(left1);
+    right2.follow(right1);
+    //RelativeEncoder leftEncoder = left1.getEncoder(); // used for brushless motors
+   // RelativeEncoder rightEncoder = right1.getEncoder();
 
     this.leftSupplier = leftSupplier;
     this.rightSupplier = rightSupplier;
 
+    pid1 = left1.getPIDController();
+    pid1.setP(Math.PI); // set p to pi
+
+    endoder1 = left1.getEncoder();
+
+
     SmartDashboard.putData(field);
   }
 
-  /**
+  /**s
    * Example command factory method.
    *
    * @return a command
@@ -56,22 +76,47 @@ public class Drive extends SubsystemBase {
     return run(() -> drive(leftSupplier, rightSupplier));
   }
 
+  public Command drivebackCommand(double speedys, double spinnys) {
+    // Inline construction of command goes here.
+    // Subsystem::RunOnce implicitly requires `this` subsystem.
+    return run(() -> driveback(speedys, spinnys)); // (speed foward/back, right/left rotation)
+  }
+
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-    SmartDashboard.putNumber("Left Motor Position", Util.getLeftDistance());
-    SmartDashboard.putNumber("Right Motor Position", Util.getRightDistance());
+    //SmartDashboard.putNumber("Left Motor Position", leftEncoder.getPosition());  // puts the motors position using the encoder position on the dash board
+    //SmartDashboard.putNumber("Right Motor Position", rightEncoder.getPosition());  // puts the motors position using the encoder position on the dash board
 
   }
 
   public void drive(DoubleSupplier leftSupplier, DoubleSupplier rightSupplier) {
-    left.set(-leftSupplier.getAsDouble() + rightSupplier.getAsDouble());
-    right.set(-leftSupplier.getAsDouble() - rightSupplier.getAsDouble());
+    // TODO: Implement this method, to set the motors to the throttle values from
+    // the joystick
+    // left.set(leftSupplier.getAsDouble());
+    // right.set(rightSupplier.getAsDouble());
+    left1.set(-leftSupplier.getAsDouble() - -rightSupplier.getAsDouble());
+    right1.set(-leftSupplier.getAsDouble() + - rightSupplier.getAsDouble());
+    // TODO: (Optional) Implement Arcade (One Stick) Driving
+    // Y Axis controls forward motion, X Axis controls rotation
+
   }
+    public void driveback(double left, double right) {
+    // TODO: Implement this method, to set the motors to the throttle values from
+    // the joystick
+    // left.set(leftSupplier.getAsDouble());
+    // right.set(rightSupplier.getAsDouble());
+    left1.set(-left + right);
+    right1.set(-left - right);
+    // TODO: (Optional) Implement Arcade (One Stick) Driving
+    // Y Axis controls forward motion, X Axis controls rotation
+
+  }
+
 
   @Override
   public void simulationPeriodic() {
     // This method will be called once per scheduler run during simulation
-  
+
   }
 }
--- RobotProject/ProgrammingIntro2024/src/main/java/frc/robot/subsystems/Intake.java
+++ RobotProject/ProgrammingIntro2024/src/main/java/frc/robot/subsystems/Intake.java
@@ -7,39 +7,43 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class intake extends SubsystemBase {
-    CANSparkMax intakeMotor;
+public class Intake extends SubsystemBase {
+  /** Creates a new intake. */
 
-    public intake() {
-        intakeMotor = new CANSparkMax(8, MotorType.kBrushless);
-    }
+  CANSparkMax intakeMotor;
 
-    public void intakingNote() {
-        intakeMotor.set(1);
-    }
+  public Intake() {
 
-    public void intakeExhaustion() {
-        intakeMotor.set(-1);
-    }
+    intakeMotor = new CANSparkMax(6, MotorType.kBrushless);
 
-    public void stop() {
-        intakeMotor.set(0);
-    }
+  }
 
-    public Command getIntakeCommand() {
-        return new RunCommand(
-                () -> {
-                    intakingNote();
-                })
-                .finallyDo(() -> stop());
-    }
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
 
-    public Command getExhaustionCommand() {
-        return new RunCommand(
-                () -> {
-                    intakeExhaustion();
-                })
-                .finallyDo(() -> stop());
-    }
 
+
+  public void stop() {
+    intakeMotor.setVoltage(0);
+  }
+
+  public void intakeGround() {
+    intakeMotor.setVoltage(-6); // intake from the ground
+  }
+
+  public void exhaustNote() {
+    intakeMotor.setVoltage(+6); // rid of note
+  }
+
+  public Command getIntakeCommand() {
+    return new RunCommand(() -> { intakeGround(); // supplier to send the intake from ground command
+    }) .finallyDo(() -> stop());  // when it is not in use it "stops"
+  }
+
+  public Command getExhaustNoteCommand(){
+      return new RunCommand( () -> { exhaustNote(); })  // supplier to send the "exhaustNote" command
+      .finallyDo(() -> stop());  // when it is not in use it "stops"
+  }
 }
